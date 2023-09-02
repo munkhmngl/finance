1c2bbd1b5b08d568db91e23d3a2d0d1fef447d11
munkhmngl <munkhmngl@gmail.com>
2023-09-02 23:30:05 +0800

    Эхлэх функц бичлээ

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function() {
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: '.add__btn'
+    }
 
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {
@@ -8,21 +26,35 @@ var financeController = (function() {
 })();
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
-    var ctrlAddItem = function() {
+    let ctrlAddItem = function() {
         // Оруулах өгөгдлийг дэлгэцнээё олж авна
+        console.log(uiController.getInput());
         // Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулна
         // Олж авсан өгөгдлүүдээ тохирох хэсэгт нь гаргана
         // Төсвийг тооцоолно
         // Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана
-    }
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        ctrlAddItem();
-    });
+    };
 
-    document.addEventListener('keypress', function(e){
-        if(e.key === 13 || e.which === 13){
+    let setupEventListeners = function(){
+        let DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.addBtn).addEventListener('click', function(){
             ctrlAddItem();
+        });
+    
+        document.addEventListener('keypress', function(e){
+            if(e.key === 13 || e.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+
+    return {
+        init: function() {
+            console.log('Программ эхлэх...');
+            setupEventListeners();
         }
-    });
+    }
 
 })(uiController, financeController);
+
+appController.init();

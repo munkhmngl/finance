3e2a49ad7355c7b2ed701aeaac0784d0c07bd197
munkhmngl <munkhmngl@gmail.com>
2023-09-03 04:45:08 +0800

    Санхүүгийн хувьсагчдыг үүсгэв

--- app.js
+++ app.js
@@ -22,6 +22,26 @@ var uiController = (function() {
 })();
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {
+    let Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    let Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+    let data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    };
     
 })();
 // Програмын холбогч контроллер

b2ecd79b1299ef4d18eab9f36e7da486f0065021
munkhmngl <munkhmngl@gmail.com>
2023-09-05 17:11:39 +0800

    Мөнгийг харагдах байдлыг засварлав

--- app.js
+++ app.js
@@ -1,232 +1,273 @@
-// Дэлгэцтэй ажиллах контроллер
+// Ð”ÑÐ»Ð³ÑÑ†Ñ‚ÑÐ¹ Ð°Ð¶Ð¸Ð»Ð»Ð°Ñ… ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
 var uiController = (function() {
-    let DOMstrings = {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
-        addBtn: '.add__btn',
-        incomeList: '.income__list',
-        expenseList: '.expenses__list',
-        tusuvLabel: '.budget__value',
-        incomeLabel: '.budget__income--value',
-        expenseLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage',
-        containerDiv: '.container'
-    }
-
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list",
+        tusuvLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expeseLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage",
+        containerDiv: ".container",
+        expensePercentageLabel: ".item__percentage",
+        dateLabel: '.budget__title--month'
+    };
+    var nodelistForeach = function(list, callback) {
+        for(let i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+    var formatMoney = function(number, type){
+        number = '' + number;
+        var tempA = number.split('').reverse().join('');
+        var tempB = '', count = 1;
+        for(let i = 0; i < tempA.length; i++) {
+            tempB += tempA[i];
+            if(count % 3 === 0) tempB += ',';
+            count++;
+        }
+        var tempC = tempB.split('').reverse().join('');
+        if(tempC[0] === ',') tempC = tempC.substring(1, tempC.length);
+        if(type === 'inc') tempC = '+ ' + tempC;
+        else tempC = '- ' + tempC;
+        return tempC;
+    };
     return {
-        getMonth: function(){
-            let month = new Date().getMonth();
-            document.querySelector('.budget__title--month').insertAdjacentElement('beforeend', month);
-            console.log(month);
+        displayDate: function(){
+            var date = new Date();
+            document.querySelector(DOMstrings.dateLabel).textContent = date.getFullYear() + ' оны ' + date.getMonth() + ' сарын ӨРХИЙН санхүү';
         },
-        getInput: function(){
+        getInput: function() {
             return {
-                type: document.querySelector(DOMstrings.inputType).value,
+                type: document.querySelector(DOMstrings.inputType).value, // exp, inc
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseInt(document.querySelector(DOMstrings.inputValue).value)
             };
         },
-        getDOMstrings: function(){
+        displayPersentages: function(allPercentages){
+            // зарлагын NodeList-ийг олох <buttom id=''
+            var elements = document.querySelectorAll(DOMstrings.expensePercentageLabel);
+            // Элемент болгоны хувьд зарлагын хувийг массиваас авч шивж оруулах
+            nodelistForeach(elements, function(el, index){
+                el.textContent = allPercentages[index] + '%';
+            });
+        },
+        getDOMstrings: function() {
             return DOMstrings;
         },
-        clearFields: function(){
-            let fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+        clearFields: function() {
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
             // Convert List to Array
             var fieldsArr = Array.prototype.slice.call(fields);
-            fieldsArr.forEach(el => {
-                el.value = '';
+            fieldsArr.forEach(function(el) {
+                el.value = "";
             });
             fieldsArr[0].focus();
         },
-        dataLooked: function(data){
-            document.querySelector(DOMstrings.tusuvLabel).textContent = data.tusuv;
-            document.querySelector(DOMstrings.incomeLabel).textContent = data.totalInc;
-            document.querySelector(DOMstrings.expenseLabel).textContent = data.totalExp;
-            if(data.huvi !== 0) document.querySelector(DOMstrings.percentageLabel).textContent = data.huvi + '%';
-            else document.querySelector(DOMstrings.percentageLabel).textContent = data.huvi;
+        tusviigUzuuleh: function(tusuv) {
+            let type;
+            if(tusuv.tusuv >= 0) type = 'inc';
+            document.querySelector(DOMstrings.tusuvLabel).textContent = formatMoney(tusuv.tusuv, type);
+            document.querySelector(DOMstrings.incomeLabel).textContent = '+ ' + tusuv.totalInc;
+            document.querySelector(DOMstrings.expeseLabel).textContent = '- ' + tusuv.totalExp;
+            if (tusuv.huvi !== 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi + "%";
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi;
+            }
         },
-        deleteListItem: function(id){
-            let el = document.getElementById(id);
+        deleteListItem: function(id) {
+            var el = document.getElementById(id);
             el.parentNode.removeChild(el);
         },
-        addListItem: function(item, type){
-            // Орлого зарлагыи элементийг агуулсан html-ийг бэлтгэнэ
-            let html, list;
-            if(type === 'inc'){
+        addListItem: function(item, type) {
+            // ÐžÑ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¸Ð¹Ð³ Ð°Ð³ÑƒÑƒÐ»ÑÐ°Ð½ html-Ð¸Ð¹Ð³ Ð±ÑÐ»Ñ‚Ð³ÑÐ½Ñ.
+            var html, list;
+            if (type === "inc") {
                 list = DOMstrings.incomeList;
-                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESXRIPTIN$$</div><div class="right clearfix"><div class="item__value">+ $$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else {
                 list = DOMstrings.expenseList;
-                html ='<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESXRIPTIN$$</div><div class="right clearfix"><div class="item__value">- $$VALUE$$</div><div class="item__percentage">%huvi%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-            // Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE ашиглаж өөрчилж өгнө
-            html = html.replace('%id%', item.id);
-            html = html.replace('$$DESXRIPTIN$$', item.description);
-            html = html.replace('$$VALUE$$', item.value);
-            let data = financeController.tusuvTuluv();
-            html = html.replace('%huvi%', data + '%');
-            // Бэлтгэсэн HTML ээ DOM руу хийж өгнө
-            document.querySelector(list).insertAdjacentHTML('beforeend', html);
+            // Ð¢ÑÑ€ HTML Ð´Ð¾Ñ‚Ñ€Ð¾Ð¾ Ð¾Ñ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ ÑƒÑ‚Ð³ÑƒÑƒÐ´Ñ‹Ð³ REPLACE Ð°ÑˆÐ¸Ð³Ð»Ð°Ð¶ Ó©Ó©Ñ€Ñ‡Ð¸Ð»Ð¶
+            html = html.replace("%id%", item.id);
+            html = html.replace("$$DESCRIPTION$$", item.description);
+            html = html.replace("$$VALUE$$", formatMoney(item.value, type));
+            // Ð‘ÑÐ»Ñ‚Ð³ÑÑÑÐ½ HTML ÑÑ DOM Ñ€ÑƒÑƒ Ñ…Ð¸Ð¹Ð¶ Ó©Ð³Ð½Ó©.
+            document.querySelector(list).insertAdjacentHTML("beforeend", html);
         }
     };
 })();
-// Санхүүтэй ажиллах контроллер
+// Ð¡Ð°Ð½Ñ…Ò¯Ò¯Ñ‚ÑÐ¹ Ð°Ð¶Ð¸Ð»Ð»Ð°Ñ… ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
 var financeController = (function() {
-    let Income = function(id, description, value){
+    // private data
+    var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
-    let Expense = function(id, description, value){
+    // private data
+    var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
     };
-    Expense.prototype.calcPercentage = function(totalIncome){
-        if(totalIncome > 0)
-        this.percentage = Math.round((this.value / totalIncome) * 100);
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0)
+            this.percentage = Math.round((this.value / totalIncome) * 100);
         else this.percentage = 0;
     };
-    Expense.prototype.getPercentage = function(){
+    Expense.prototype.getPercentage = function() {
         return this.percentage;
     };
-    let calculateTotal = function(type){
-        let sum = 0;
-        data.items[type].forEach(el => {
-            sum += el.value;
-        });
-        data.totals[type] = sum;
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.items[type].forEach(function(el) {
+        sum = sum + el.value;
+    });
+    data.totals[type] = sum;
     };
-    let data = {
+    // private data
+    var data = {
         items: {
             inc: [],
             exp: []
         },
-        totals: {
-            inc: 0,
-            exp: 0
-        },
-        tusuv : 0,
-        huvi: 0
+    totals: {
+        inc: 0,
+        exp: 0
+    },
+    tusuv: 0,
+    huvi: 0
     };
     return {
-        tusuvTuluv: function(){
-            calculateTotal('inc');
-            calculateTotal('exp');
+        tusuvTootsooloh: function() {
+            // ÐÐ¸Ð¹Ñ‚ Ð¾Ñ€Ð»Ð¾Ð³Ñ‹Ð½ Ð½Ð¸Ð¹Ð»Ð±ÑÑ€Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+            calculateTotal("inc");
+            // ÐÐ¸Ð¹Ñ‚ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ Ð½Ð¸Ð¹Ð»Ð±ÑÑ€Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+            calculateTotal("exp");
+            // Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ ÑˆÐ¸Ð½ÑÑÑ€ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
             data.tusuv = data.totals.inc - data.totals.exp;
-            if(data.tusuv.inc > 0)
+            // ÐžÑ€Ð»Ð¾Ð³Ð¾ Ð·Ð°Ñ€Ð»Ð°Ð³Ñ‹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+            if (data.totals.inc > 0)
                 data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
             else data.huvi = 0;
-            let zardalHuvi = Math.round((data.totals.exp / data.tusuv) * 100);
-            return zardalHuvi;
         },
-        calculatePercentage: function(){
-            data.items.exp.forEach( function(el){
+        calculatePercentages: function() {
+            data.items.exp.forEach(function(el) {
                 el.calcPercentage(data.totals.inc);
             });
         },
-        getPercentage: function() {
-            let allPercentage = data.items.exp.map(function(el){
+        getPercentages: function() {
+            var allPercentages = data.items.exp.map(function(el) {
                 return el.getPercentage();
             });
-            return allPercentage;
+            return allPercentages;
         },
-        tusuvEnd: function(){
+        tusviigAvah: function() {
             return {
                 tusuv: data.tusuv,
                 huvi: data.huvi,
                 totalInc: data.totals.inc,
                 totalExp: data.totals.exp
-            }
+            };
         },
-        deleteItem: function(type, id){
-            let ids = data.items[type].map(function(el){
+        deleteItem: function(type, id) {
+            var ids = data.items[type].map(function(el) {
                 return el.id;
             });
-            let index = ids.indexOf(id);
-            if(index !== -1){
+            var index = ids.indexOf(id);
+            if (index !== -1) {
                 data.items[type].splice(index, 1);
             }
         },
-        addItem: function(type, desc, val){
-            let item, id;
-            data.items[type].length === 0 ? id = 1 : id = data.items[type][data.items[type].length - 1].id + 1;
-            type === 'inc' ? item = new Income(id, desc, val) : item = new Expense(id, desc, val);
+        addItem: function(type, desc, val) {
+            var item, id;
+            if (data.items[type].length === 0) id = 1;
+            else id = data.items[type][data.items[type].length - 1].id + 1;
+            if (type === "inc") item = new Income(id, desc, val);
+            else item = new Expense(id, desc, val);
             data.items[type].push(item);
             return item;
+        },
+        seeData: function() {
+            return data;
         }
-    }
+    };
 })();
-// Програмын холбогч контроллер
+// ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼Ñ‹Ð½ Ñ…Ð¾Ð»Ð±Ð¾Ð³Ñ‡ ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€
 var appController = (function(uiController, financeController) {
-    let ctrlAddItem = function() {
-        // Оруулах өгөгдлийг дэлгэцнээс олж авна
-        let input = uiController.getInput();
-        if(input.description !== '' && input.value !== ''){
-            // Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулна
-            let item = financeController.addItem(input.type, input.description, parseInt(input.value));
-            // Олж авсан өгөгдлүүдээ тохирох хэсэгт нь гаргана
+    var ctrlAddItem = function() {
+        // 1. ÐžÑ€ÑƒÑƒÐ»Ð°Ñ… Ó©Ð³Ó©Ð³Ð´Ð»Ð¸Ð¹Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÑÑ Ð¾Ð»Ð¶ Ð°Ð²Ð½Ð°.
+        var input = uiController.getInput();
+        if (input.description !== "" && input.value !== "") {
+            // 2. ÐžÐ»Ð¶ Ð°Ð²ÑÐ°Ð½ Ó©Ð³Ó©Ð³Ð´Ð»Ò¯Ò¯Ð´ÑÑ ÑÐ°Ð½Ñ…Ò¯Ò¯Ð³Ð¸Ð¹Ð½ ÐºÐ¾Ð½Ñ‚Ñ€Ð¾Ð»Ð»ÐµÑ€Ñ‚ Ð´Ð°Ð¼Ð¶ÑƒÑƒÐ»Ð¶ Ñ‚ÑÐ½Ð´ Ñ…Ð°Ð´Ð³Ð°Ð»Ð½Ð°.
+            var item = financeController.addItem(input.type,input.description,input.value);
+            // 3. ÐžÐ»Ð¶ Ð°Ð²ÑÐ°Ð½ Ó©Ð³Ó©Ð³Ð´Ð»Ò¯Ò¯Ð´ÑÑ Ð²ÑÐ± Ð´ÑÑÑ€ÑÑ Ñ‚Ð¾Ñ…Ð¸Ñ€Ð¾Ñ… Ñ…ÑÑÑÐ³Ñ‚ Ð½ÑŒ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°
             uiController.addListItem(item, input.type);
             uiController.clearFields();
-            // төсөвийг шинээр тооцоолоод дэлгэцэнд үзүүлэх
+            // Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ ÑˆÐ¸Ð½ÑÑÑ€ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð¾Ð¾Ð´ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ò¯Ð·Ò¯Ò¯Ð»Ð½Ñ.
             updateTusuv();
         }
     };
-    let updateTusuv = function() {
-        // Төсвийг тооцоолно
-        financeController.tusuvTuluv();
-        // Эцсийн үлдэгдэл
-        let oldData = financeController.tusuvEnd();
-        //тооцоог дэлгэцэнд гаргана
-        uiController.dataLooked(oldData);
-        // Элементүүдийн хувийг тооцоолно
-        financeController.calculatePercentage();
-        // элементүүдийн хувийг хүлээж авна
-        let allPercentages = financeController.getPercentage();
-        // эдгээр хувийг дэлгэцэнд гаргана
-        console.log(allPercentages);
+    var updateTusuv = function() {
+        // 4. Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+        financeController.tusuvTootsooloh();
+        // 5. Ð­Ñ†ÑÐ¸Ð¹Ð½ Ò¯Ð»Ð´ÑÐ³Ð´ÑÐ»,
+        var tusuv = financeController.tusviigAvah();
+        // 6. Ð¢Ó©ÑÐ²Ð¸Ð¹Ð½ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°.
+        uiController.tusviigUzuuleh(tusuv);
+        // 7. Ð­Ð»ÐµÐ¼ÐµÐ½Ñ‚Ò¯Ò¯Ð´Ð¸Ð¹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð½Ð¾
+        financeController.calculatePercentages();
+        // 8. Ð­Ð»ÐµÐ¼ÐµÐ½Ñ‚Ò¯Ò¯Ð´Ð¸Ð¹Ð½ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ñ…Ò¯Ð»ÑÑÐ¶ Ð°Ð²Ð½Ð°
+        var allPercentages = financeController.getPercentages();
+         // 9. Ð­Ð´Ð³ÑÑÑ€ Ñ…ÑƒÐ²Ð¸Ð¹Ð³ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ð³Ð°Ñ€Ð³Ð°Ð½Ð°.
+        uiController.displayPersentages(allPercentages);
     };
-    let setupEventListeners = function(){
-        let DOM = uiController.getDOMstrings();
-        document.querySelector(DOM.addBtn).addEventListener('click', function(){
+    var setupEventListeners = function() {
+        var DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.addBtn).addEventListener("click", function() {
+        ctrlAddItem();
+    });
+    document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
-        });
-        document.addEventListener('keypress', function(e){
-            if(e.key === 13 || e.which === 13){
-                ctrlAddItem();
-            }
-        });
-        document.querySelector(DOM.containerDiv).addEventListener('click', function(e){
-            let id = e.target.parentNode.parentNode.parentNode.parentNode.id;
-            if(id){
-                let arr = id.split('-');
-                let type = arr[0];
-                let itemId = parseInt(arr[1]);
-                // console.log(type + ' : ' + itemId);
-                // санхүүгийн модулиас type, id ашиглаад устгана
-                financeController.deleteItem(type, itemId);
-                // дэлгэц дээрээс энэ элементийг устгана
-                uiController.deleteListItem(id);
-                // үлдэгдэл тооцоог шинэчилж харуулна
-                updateTusuv();
+        }
+    });
+    document.querySelector(DOM.containerDiv).addEventListener("click", function(event) {
+        var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if (id) {
+            // inc-2
+            var arr = id.split("-");
+            var type = arr[0];
+            var itemId = parseInt(arr[1]);
+            console.log(type + " ===> " + itemId);
+            // 1. Ð¡Ð°Ð½Ñ…Ò¯Ò¯Ð³Ð¸Ð¹Ð½ Ð¼Ð¾Ð´ÑƒÐ»Ð¸Ð°Ñ type, id Ð°ÑˆÐ¸Ð³Ð»Ð°Ð°Ð´ ÑƒÑÑ‚Ð³Ð°Ð½Ð°.
+            financeController.deleteItem(type, itemId);
+            // 2. Ð”ÑÐ»Ð³ÑÑ† Ð´ÑÑÑ€ÑÑÑ ÑÐ½Ñ ÑÐ»ÐµÐ¼ÐµÐ½Ñ‚Ð¸Ð¹Ð³ ÑƒÑÑ‚Ð³Ð°Ð½Ð°
+            uiController.deleteListItem(id);
+            // 3. Ò®Ð»Ð´ÑÐ³Ð´ÑÐ» Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð³ ÑˆÐ¸Ð½ÑÑ‡Ð¸Ð»Ð¶ Ñ…Ð°Ñ€ÑƒÑƒÐ»Ð½Ð°.
+            // Ð¢Ó©ÑÐ²Ð¸Ð¹Ð³ ÑˆÐ¸Ð½ÑÑÑ€ Ñ‚Ð¾Ð¾Ñ†Ð¾Ð¾Ð»Ð¾Ð¾Ð´ Ð´ÑÐ»Ð³ÑÑ†ÑÐ½Ð´ Ò¯Ð·Ò¯Ò¯Ð»Ð½Ñ.
+            updateTusuv();
             }
         });
     };
-
     return {
         init: function() {
-            console.log('Программ эхлэх...');
-            uiController.dataLooked({
-                tusuv: 0,
-                huvi: 0,
-                totalInc: 0,
-                totalExp: 0
-            });
-            setupEventListeners();
+            uiController.displayDate();
+            uiController.tusviigUzuuleh({
+            tusuv: 0,
+            huvi: 0,
+            totalInc: 0,
+            totalExp: 0
+        });
+        setupEventListeners();
         }
-    }
-
+    };
 })(uiController, financeController);
-
 appController.init();
